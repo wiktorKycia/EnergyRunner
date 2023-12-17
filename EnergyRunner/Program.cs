fd15d06998f024bd2a63d44792265279027abfd1
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-12-17 20:03:38 +0100

    first commit

---
+++ EnergyRunner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EnergyRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

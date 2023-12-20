8e5212b9309381faf12d8d188838a50586e1f3c8
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-12-20 18:11:29 +0100

    changing console output encoding to unicode and testing it

--- EnergyRunner/Program.cs
+++ EnergyRunner/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("\u2022; \u2606; \u2605; \u2B50"); 
         }
     }
 }

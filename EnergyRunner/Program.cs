1831656d73906703d21f9ba7eaf6cd2dd68ee243
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-12-20 18:59:17 +0100

    add home char to console writeline at the beginning of the program

--- EnergyRunner/Program.cs
+++ EnergyRunner/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("\u2022; \u2606; \u2605; \u2B50");
+            Console.WriteLine("\u2022; \u2606; \u2605; \u2B50; \u2302");
         }
     }
     class Player
